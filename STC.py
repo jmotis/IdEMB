d49e8265ffd02bcd872a35cc281363627f3da986
MegJBrown <meaghan.j.brown@gmail.com>
2016-07-03 09:12:32 -0400

    Add files via upload
    Uploading most productive, least noise-generative versions of search for finding repositoires

---
+++ STC.py
@@ -0,0 +1,53 @@
+# This is a file for me to start playing with code to process the JSON
+
+def find_librarians():
+    # load libraries
+    import re
+    
+    # set search term, create and write header to output file, initialize index
+    results = open('resultsSTC.csv', 'w+')
+    results.write('JSON DOI,Statement_Type,Snippet\n')
+    i = 0
+    
+    # for each JSON file
+    while (i<7):
+        filename = 'data-0000'+str(i)+'-research-articles.json'
+        text = open(filename, 'r')
+        entry = text.readlines()
+    
+        # for each line in the file
+        for line in entry:
+            # find article ID
+            start_ID_index = line.find('"doi": "') + 8
+            end_ID_index = line.find('"}')
+            ID = line[start_ID_index:end_ID_index]
+
+        
+            # find permission statements
+            statement_type = 'STC'
+            start_index = 0
+            for mention in re.finditer('STC', line, flags=re.IGNORECASE):
+                start_index = mention.start()
+                if (start_index > 0):
+                    snippet = line[start_index-75:start_index+30]
+                    results.write(ID + ',' + statement_type + ',' + '"' + snippet + '"' + '\n')
+              # find permission statements
+            statement_type = 'Wing'
+            start_index = 0
+            for mention in re.finditer(' Wing', line):
+                start_index = mention.start()
+                if (start_index > 0):
+                    snippet = line[start_index-75:start_index+12]
+                    results.write(ID + ',' + statement_type + ',' + '"' + snippet + '"' + '\n')
+      
+    		
+        text.close
+        i = i + 1
+
+    # close the results file
+    results.close
+# end function
+
+if __name__ == "__main__":
+  find_librarians()
+  print "The function to find statements about archives, libraries, repositories, etc. has run. WARNING: If you do not rename results.csv before re-running this program, the results.csv file will be overwritten."
---
+++ debt.py
@@ -0,0 +1,51 @@
+# This is a file for me to start playing with code to process the JSON
+
+def find_librarians():
+    # load libraries
+    import re
+    
+    # set search term, create and write header to output file, initialize index
+    results = open('resultsDebt.csv', 'w+')
+    results.write('JSON DOI,Statement_Type,Snippet\n')
+    i = 0
+    
+    # for each JSON file
+    while (i<7):
+        filename = 'data-0000'+str(i)+'-research-articles.json'
+        text = open(filename, 'r')
+        entry = text.readlines()
+    
+        # for each line in the file
+        for line in entry:
+            # find article ID
+            start_ID_index = line.find('"doi": "') + 8
+            end_ID_index = line.find('"}')
+            ID = line[start_ID_index:end_ID_index]
+
+        
+            # find permission statements
+            statement_type = 'debt'
+            start_index = 0
+            for mention in re.finditer('indebted to', line, flags=re.IGNORECASE):
+                start_index = mention.start()
+                if (start_index > 0):
+                    snippet = line[start_index-65:start_index+65]
+                    results.write(ID + ',' + statement_type + ',' + '"' + snippet + '"' + '\n')
+            start_index = 0
+            for mention in re.finditer('following libraries', line, flags=re.IGNORECASE):
+                start_index = mention.start()
+                if (start_index > 0):
+                    snippet = line[start_index-25:start_index+105]
+                    results.write(ID + ',' + statement_type + ',' + '"' + snippet + '"' + '\n')
+      
+    		
+        text.close
+        i = i + 1
+
+    # close the results file
+    results.close
+# end function
+
+if __name__ == "__main__":
+  find_librarians()
+  print "The function to find statements about archives, libraries, repositories, etc. has run. WARNING: If you do not rename results.csv before re-running this program, the results.csv file will be overwritten."
